f97331002bb8de801e88aaa71d6ddbfad41b8c3d
RUYANGA <ruyangam15@gmail.com>
2025-11-10 17:20:32 +0200

    fix cros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from "@nestjs/common";
 
 import { AuthModule } from "./auth/auth.module";
+import { MealModule } from "./meal/meal.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { InfrastructureModule } from "./share/infrastructure/infrastructure.module";
 import { UserModule } from "./user/user.module";
-import { MealModule } from './meal/meal.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, InfrastructureModule, MealModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    InfrastructureModule,
+    MealModule,
+  ],
 })
 export class AppModule {}
